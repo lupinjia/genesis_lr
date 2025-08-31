c4fa8a64361d0febd3acdfae6b1ec01b16d320e8
lupinjia <jiayasen021003@gmail.com>
2025-08-31 23:56:22 +0800

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -10,9 +10,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
 
 
 # -- Project information -----------------------------------------------------
@@ -62,7 +62,8 @@ html_context = {
     "github_user": "lupinjia",
     "github_repo": "genesis_lr",
     "github_version": "dev",
-    "conf_py_path": "./",
+    "conf_py_path": "/source/",
+    "doc_path": "/source",
 }
 html_theme_options = {
     "show_nav_level": 2,
