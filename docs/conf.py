0bc3709eaa4c63b3623d1b4b4a84353def8acdff
lupinjia <jiayasen021003@gmail.com>
2025-08-31 22:08:36 +0800

    update doc

--- docs/conf.py
+++ docs/conf.py
@@ -31,33 +31,46 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_copybutton",
+    "myst_parser",
+    "sphinx_subfigure",
+    "sphinxcontrib.video",
+    "sphinx_togglebutton",
+    "sphinx_design",
 ]
 
-intersphinx_mapping = {
-    "rtd": ("https://docs.readthedocs.io/en/stable/", None),
-    "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
-}
-intersphinx_disabled_domains = ["std"]
+# https://myst-parser.readthedocs.io/en/latest/syntax/optional.html
+myst_enable_extensions = ["colon_fence", "dollarmath", "amsmath", "dollarmath"]
+# https://github.com/executablebooks/MyST-Parser/issues/519#issuecomment-1037239655
+myst_heading_anchors = 4
 
 templates_path = ["_templates"]
-
-# -- Options for EPUB output
-epub_show_urls = "footnote"
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
-html_theme = "sphinx_rtd_theme"
+html_theme = "pydata_sphinx_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
+html_context = {
+    "display_github": True,
+    "github_user": "lupinjia",
+    "github_repo": "genesis_lr",
+    "github_version": "main",
+    "conf_py_path": "/",
+    "doc_path": "/",
+}
 html_static_path = ["_static"]
+
+### Autodoc configurations ###
+autodoc_typehints = "signature"
+autodoc_typehints_description_target = "all"
+autodoc_default_flags = ["members", "show-inheritance", "undoc-members"]
+autodoc_member_order = "bysource"
+autosummary_generate = True
